6fb472667de54515a88c34c710fe54b71172008d
Jacobzzang <89247402+Jacobzzang@users.noreply.github.com>
2021-11-08 02:52:02 +0900

    kdg 1108 0251

--- Dolvomee/src/main/java/co/yedam/dolvomee/web/FrontController.java
+++ Dolvomee/src/main/java/co/yedam/dolvomee/web/FrontController.java
@@ -36,9 +36,21 @@ public class FrontController extends HttpServlet {
 	}
 
 	public void init(ServletConfig config) throws ServletException {
-
+		
+		//준형
 		map.put("/home.do", new HomeCommand()); // 첫 페이지 호출
+		
+		//동관
 		map.put("/reviewList.do", new ReviewList()); //리뷰목록
+		map.put("/reviewForm.do", new ReviewForm()); //리뷰 등록
+		map.put("/reviewSelect.do", new ReviewSelect()); //리뷰 상세페이지
+		map.put("/reviewEdit.do", new ReviewEdit()); //리뷰 수정
+		map.put("/qnaList.do", new QnaList()); //1:1 문의
+		
+		//은진
+		
+		//선영
+		
 		map.put("/usersLoginForm.do", new UsersLoginForm()); // 로그인 폼 호출
 		map.put("/usersLogin.do", new UsersLogin()); // 로그인 처리
 		map.put("/usersRegisterForm.do", new UsersRegisterForm()); // 사용자회원가입 폼 호출
@@ -46,9 +58,6 @@ public class FrontController extends HttpServlet {
 		map.put("/dolvRegisterForm.do", new DolvRegisterForm()); // 돌보미 회원가입 폼 호출 
 		map.put("/usersList.do", new UsersList()); //회원 리스트(관리자만)
 		map.put("/dolvomeeList", new DolvomeeList()); //펫시터 리스트(관리자만)
-		map.put("/qnaList.do", new QnaList()); //1:1 문의
-		map.put("/reviewForm.do", new ReviewForm()); //리뷰 등록
-		map.put("/reviewSelect.do", new ReviewSelect()); //리뷰 상세페이지
 		map.put("/serviceList.do", new ServiceList()); // 돌보미 서비스 리스트
 	}
 
---
+++ Dolvomee/src/main/java/co/yedam/dolvomee/web/ReviewEdit.java
@@ -0,0 +1,17 @@
+package co.yedam.dolvomee.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.yedam.dolvomee.comm.Command;
+
+public class ReviewEdit implements Command {
+
+	@Override
+	public String run(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		return null;
+	}
+
+}
